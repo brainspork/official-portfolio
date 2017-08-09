7b54fe2cd9b29b664d95c2480c5c7607203327a7
brainspork <silv.amee@gmail.com>
2017-08-08 19:41:26 -0700

    Styles mobile view for landing
    Establishes basic design for site. Adds style to header footer and home page

--- public/app/main-nav/main-nav.component.js
+++ public/app/main-nav/main-nav.component.js
@@ -3,5 +3,13 @@
 angular.
   module('mainNav').
     component('mainNav', {
-      templateUrl: 'app/main-nav/main-nav.template.html'
+      templateUrl: 'app/main-nav/main-nav.template.html',
+      controller: function navigationController () {
+        var vm = this;
+
+        vm.showNav = function () {
+          $('nav').toggleClass('show-nav');
+          $('body').toggleClass('scroll-stop');
+        }
+      }
     });
--- server.js
+++ server.js
@@ -9,9 +9,3 @@ app.use('/script', express.static(`${__dirname}/node_modules/`));
 app.use(express.static('./public'));
 
 app.listen(PORT, () => console.log(`You are on port: ${PORT}. Enjoy the Storm.`));
-
-const conString = 'postgress://postgres:Jesus69!@localhost:5432/mtgfinder';
-const client = new pg.Client(conString);
-
-client.connect();
-client.on('error', err => console.error(err));
